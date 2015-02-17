c81734c0edeb282dca4eaba74fc25d1a01c9f27a
inaimathi <inaimathi@self>
2015-02-16 23:53:59 -0500

    -Defined preliminary version of load-module -Need to think about `module` itself

--- modules.lisp
+++ modules.lisp
@@ -8,33 +8,55 @@
 (defun install-module (name &key server)
   ;; check install servers for given module. Install if they existz
   nil)
+
 (defun upgrade-module (name &key server) 
   ;; check install servers for new versions. Install them if they exist.
   nil)
-(defun load-module (name) 
-  ;; if in *strap-wolf*, load it from there
-  ;; otherwise check some install servers
-  ;; otherwise fail
-  nil)
+
+(defun load-module (name)
+  (let ((mod (first
+	      (sort 
+	       (for-all 
+		`(and (?id :name ,(->wolf-name name))
+		      (?id :parameters ?args) (?id :source ?body)
+		      (?id :registered ?stamp))
+		:in *strap-wolf* 
+		:collect (list ?stamp ?args ?body))
+	       #'> :key #'first))))
+    (if mod 
+	(eval `(module ,name ,(second mod) ,@(third mod)))
+	(error "TODO - check install servers here. Fail if not found."))))
 
 (defmethod module-exists? ((name symbol))
-  (for-all `(?id :name ,name) :in *strap-wolf* :do (return ?id)))
+  (for-all 
+   `(?id :name ,(->wolf-name name))
+   :in *strap-wolf* :do (return ?id)))
+
+(defmethod ->wolf-name ((name symbol))
+  (list (package-name (symbol-package name)) (symbol-name name)))
 
 (defmacro module (name args &body body)
   (assert (and (listp (car body)) (null (cdr body))) nil "A module may only contain one term.")
   (assert 
    (or (eq 'container (caar body)) (lick (car body))) nil
    "A module must either be a naked CONTAINER, or a REACTOR surrounded by local-definition forms.")
-  ;; if there isn't already a function with the given name, define it.
-  (let ((alias (for-all `(and (?id :hash ,(module-hash args body)) (?id :name ?name)) :in *strap-wolf* :do (return ?name))))
-    (unless (eq alias name)
-      `(progn 
-	 ,(if alias
-	      `(alias-function ',name ',alias)
-	      `(defun ,name ,args
-		 ,@body))
-	 (register-module! ',name ',args ',body)
-	 nil))))
+  ;; TODO think about whether this actually works properly.
+  ;;   - if there's already a bound alias, just alias it, and don't bother registering
+  ;;   - if there is an alias, but it isn't bound, define the function as NAME
+  ;;   - if there's no alias, both define the function and register it
+  ;;   - if NAME is not already an alias of the appropriate module, register it as such
+  (let ((alias (for-all `(and (?id :hash ,(module-hash args body)) (?id :name ?wolf-name)) :in *strap-wolf* :do (return ?wolf-name))))
+    (cond ((and (equal alias (->wolf-name name))
+		(not (fboundp name)))
+	   `(defun ,name ,args ,@body))
+	  ((null alias)
+	   `(progn
+	      (defun ,name ,args ,@body)
+	      (register-module! ',name ',args ',body)))
+	  (t
+	   `(progn
+	      (alias-function ',name ',alias)
+	      (register-module! ',name ',args ',body))))))
 
 (defun register-module! (name args body)
   "Registers a module in the *strap-wolf* structure.
@@ -42,20 +64,22 @@ If there's already a module with the same name and hash, this is a no-op
 If there's a module with the same name, but a different hash, we add a new version
 If there's a module with the same hash, but a different name, we add an alias for that module
 Otherwise, we register a new module"
-  (let ((hash (module-hash args body)))
+  (let ((hash (module-hash args body))
+	(pkg (package-name (symbol-package name)))
+	(nm (symbol-name name)))
     (flet ((register-version! ()
 	     (multi-insert!
 	      *strap-wolf*
-	      `((:name ,name) (:parameters ,args) (:source ,body) (:hash ,hash) (:registered ,(get-universal-time))
+	      `((:name ,(->wolf-name name))
+		(:parameters ,args) (:source ,body) (:hash ,hash) (:registered ,(get-universal-time))
 		,@(mapcar (lambda (h) `(:depends-on ,h)) (find-dependencies (car body)))))
 	     (write! *strap-wolf*)))
-      (cond ((for-all `(and (?id :name ,name) (?id :hash ,hash)) :in *strap-wolf* :do (return t))
+      (cond ((for-all `(and (?id :name ,(->wolf-name name)) (?id :hash ,hash))
+		      :in *strap-wolf* :do (return t))
 	     :duplicate-module)
 	    ((for-all `(?id :hash ,hash) :in *strap-wolf* :do (return t))
 	     (let ((id (for-all `(?id :hash ,hash) :in *strap-wolf* :do (return ?id))))
-	       (insert! *strap-wolf* (list id :name name))))
-	    ;; ((for-all `(and (?id :name ,name)) :in *strap-wolf* :do (return t))
-	    ;;  (register-version!))
+	       (insert! *strap-wolf* (list id :name (->wolf-name name)))))
 	    (t (register-version!))))))
 
 (defun part-form? (thing)
@@ -93,7 +117,7 @@ There may also be container forms we'll want to handle in the future (for instan
   (flet ((single-dep (name)
 	   (first 
 	    (for-all 
-	     `(and (?id :name ,name) (?id :hash ?hash)) 
+	     `(and (?id :name ,(->wolf-name name)) (?id :hash ?hash)) 
 	     :in *strap-wolf* 
 	     :collect ?hash))))
     (when (eq (car module-body) 'container)
